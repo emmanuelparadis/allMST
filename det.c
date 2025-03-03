064bf31f118a74c4cdfd51da4b7144e4fc668e85
Emmanuel Paradis <emmanuelparadis@users.noreply.github.com>
2025-03-03 17:06:03 +0100

    added check of row/col filled with 0's

--- det.c
+++ det.c
@@ -1,4 +1,4 @@
-/* Copyright 2024 Emmanuel Paradis */
+/* Copyright 2024-2025 Emmanuel Paradis */
 
 #include <R.h>
 #include <Rinternals.h>
@@ -93,21 +93,56 @@ void foo(double *x, int *n, int *p, double *res, int *logscale, double *sign)
     }
 }
 
+int any_row_filled_with_zeros(double *x, int n, int p)
+{
+    int i, k, end = (p - 1)*n;
+    for (i = 0; i < n; i++) {
+	for (k = 0; k <= end; k += n) {
+	    if (x[k]) break;
+	}
+	if (k > end) return 1;
+	x++;
+    }
+    return 0;
+}
+
+int any_column_filled_with_zeros(double *x, int n, int p)
+{
+    int i, j;
+    for (j = 0; j < p; j++) {
+	for (i = 0; i < n; i++) {
+	    if (x[i]) break;
+	}
+	if (i == n) return 1;
+	x += n;
+    }
+    return 0;
+}
+
 /* this version does det(x) on a log-scale for x square and symmetric */
 SEXP bar(SEXP x)/* , SEXP logscale) */
 {
     int n;
-    double sign[1], *w;
+    double sign, *w, *p;
     SEXP res;
     PROTECT(x = coerceVector(x, REALSXP));
     n = nrows(x);
     if (n != ncols(x)) error("matrix not square");
-    w = (double *)R_alloc(n * n, sizeof(double));
-    /* could use duplicate() */
-    memcpy(w, REAL(x), n * n * sizeof(double));
+
     PROTECT(res = allocVector(REALSXP, 2));
-    REAL(res)[0] = det_logscale(w, n, n, sign);
-    REAL(res)[1] = sign[0];
+
+    /* test if one row or column is filled with 0 */
+    p = REAL(x);
+    if (any_column_filled_with_zeros(p, n, n) || any_row_filled_with_zeros(p, n, n)) {
+	p[0] = 0;
+	p[1] = 0;
+    } else {
+	w = (double *)R_alloc(n * n, sizeof(double));
+	memcpy(w, REAL(x), n * n * sizeof(double));
+	p[0] = det_logscale(w, n, n, &sign);
+	p[1] = sign;
+    }
+
     UNPROTECT(2);
     return res;
 }
